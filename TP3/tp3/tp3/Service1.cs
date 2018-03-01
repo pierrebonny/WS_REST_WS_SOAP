baab8ca40899421591547b2efb39142ac95520bc
Pierre Bonny <pierre.bonny@etu.unice.fr>
2018-03-01 09:34:28 +0100

    initial commit

---
+++ TP3/tp3/tp3/Service1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace tp3
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
+    public class Service1 : IService1
+    {
+        /*public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }*/
+
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "data/{id}")]
+
+        public Person GetData(string id)
+        {
+            // lookup person with the requested id 
+            return new Person()
+            {
+                Id = Convert.ToInt32(id),
+                Name = "Jean-Yves Tigli"
+            };
+        }
+    }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
